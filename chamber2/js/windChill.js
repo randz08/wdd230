5e7f2b4c8124562a16effe51d4780924fe7ea22d
randz08 <obe22001@byui.edu>
2022-05-18 23:08:35 +0800

    update

--- chamber2/js/windChill.js
+++ chamber2/js/windChill.js
@@ -1,37 +1,17 @@
+function wind_Chill_Factor(temperature, wind_Speed) {
+    let factor = (35.74 + (0.6215 * temperature) - (35.75 * Math.pow(wind_Speed, 0.16)) + (0.4275 * temperature * Math.pow(wind_Speed, 0.16)));
+    return factor;
+}
 
+var temperature = parseFloat(document.querySelector('#temperature').textContent);
+console.log(temperature);
+var wind_speed = parseFloat(document.getElementById("wind_speed").textContent);
+console.log(wind_speed);
 
+if (temperature <= 50 && wind_speed > 3) {
+    var windchill = wind_Chill_Factor(temperature, wind_speed);
+    document.querySelector("#chill").textContent = windchill.toFixed(0);
 
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsonObject) => {
-        console.log(jsonObject);
-
-        const iconURL = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
-
-        let temp = jsonObject.main.temp;
-        let speed = jsonObject.wind.speed;
-
-        document.querySelector('.cityName').textContent = jsonObject.name;
-        document.querySelector('figcaption').textContent = jsonObject.weather[0].description;
-        document.querySelector('#weatherIcon').setAttribute('src', iconURL);
-        document.querySelector('#weatherIcon').setAttribute('alt', jsonObject.weather[0].description);
-        document.querySelector('.temp').innerHTML = `Temperature: ${temp} &deg;F`;
-        document.querySelector('.wind-speed').textContent = `Wind Speed: ${speed} mph`;
-        document.querySelector('.humidityDiv').textContent = `Humidity: ${jsonObject.main.humidity}`;
-
-        let windChill = computeWindChill(temp, speed);
-
-        document.querySelector('.wind-chill').textContent = `Wind Chill: ${windChill}`;
-
-        
-    });
-
-    function computeWindChill(t, s) {
-        windchill = Math.round((35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16) * 100 / 100);
-
-        if (t <= 50 && s > 3) {
-            return windchill
-        } else {
-            return `N/A`
-        }
-    }
+} else {
+    document.querySelector("#chill").textContent = 'N/A';
+}
